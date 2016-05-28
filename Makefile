2e073d918541fda5bd32ea1b80babc84ba1ce151
wingstar74 <wingstar74@gmail.com>
2016-05-28 08:29:20 +0200

    First test program

--- Makefile
+++ Makefile
@@ -1,1 +1,5 @@
-//Makefile
+NAME = wingfence
+BOARD = arduino_nano
+
+SIMBA_ROOT = ../../simba/simba
+include $(SIMBA_ROOT)/make/app.mk
--- main.c
+++ main.c
@@ -1,1 +1,46 @@
-//First version
+/** 
+   wingfence
+   Copyright (C) 2016  Björn Lind
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+   
+   This file is part of wingfence
+   
+*/
+   
+#include "simba.h"
+
+int main()
+{
+    struct uart_driver_t uart;
+
+    /* Start the system. */
+    sys_start();
+
+    /* Initialize the UART. */
+    uart_module_init();
+    uart_init(&uart, &uart_device[0], 38400, NULL, 0);
+    uart_start(&uart);
+
+    /* Set standard output to the UART. */
+    sys_set_stdout(&uart.chout);
+
+	while (1) {
+		std_printf(FSTR("Hello world!\n"));
+		thrd_usleep(1000000); 
+	}
+	
+	return (0); 
+} 
